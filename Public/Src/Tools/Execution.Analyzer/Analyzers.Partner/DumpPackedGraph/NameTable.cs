e7ec87e924f233fdaff84f562939fe38fc3d69c0
Rob Jellinghaus <rjelling@microsoft.com>
2020-09-04 13:07:14 -0700

    Prototype code.

---
+++ Public/Src/Tools/Execution.Analyzer/Analyzers.Partner/DumpPackedGraph/NameTable.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace BuildXL.Execution.Analyzers.DumpPackedGraph
+{
+    class NameTable
+    {
+    }
+}
---
+++ Public/Src/Tools/UnitTests/Analyzers/PackedPipGraphStringTableTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Test.BuildXL.TestUtilities.Xunit;
+
+namespace Test.Tool.Analyzers
+{
+    class PackedPipGraphStringTableTests : TemporaryStorageTestBase
+    {
+    }
+}
