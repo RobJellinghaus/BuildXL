d081d1af1c57334d933e239255bad96b8a4cb0d9
Rob Jellinghaus <rjelling@microsoft.com>
2020-09-05 19:37:52 -0700

    here comes the big mama

--- Public/Src/Tools/Execution.Analyzer/Analyzers.Partner/PackedPipGraph/PackedPipGraph.cs
+++ Public/Src/Tools/Execution.Analyzer/Analyzers.Partner/PackedPipGraph/PackedPipGraph.cs
@@ -1,9 +1,49 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
+
 namespace BuildXL.Execution.Analyzers.PackedPipGraph
 {
-    class PackedPipGraph
+    /// <summary>
+    /// Overall data structure representing an entire BXL execution graph.
+    /// </summary>
+    /// <remarks>
+    /// Consists of multiple tables, with methods to construct, save, and load them all.
+    /// </remarks>
+    public class PackedPipGraph
     {
+        public readonly StringTable StringTable;
+        public readonly PipTable PipTable;
+        public readonly FileTable FileTable;
+
+        public PackedPipGraph()
+        {
+            StringTable = new StringTable();
+            PipTable = new PipTable(StringTable);
+            FileTable = new FileTable(StringTable);
+        }
+
+        public class Builder
+        {
+            public readonly PackedPipGraph PipGraph;
+            public readonly StringTable.CachingBuilder StringTableBuilder;
+            public readonly PipTable.CachingBuilder PipTableBuilder;
+            public readonly FileTable.CachingBuilder FileTableBuilder;
+
+            public Builder(PackedPipGraph pipGraph)
+            {
+                PipGraph = pipGraph;
+                StringTableBuilder = new StringTable.CachingBuilder(PipGraph.StringTable);
+                PipTableBuilder = new PipTable.CachingBuilder(PipGraph.PipTable, StringTableBuilder);
+                FileTableBuilder = new FileTable.CachingBuilder(PipGraph.FileTable, StringTableBuilder);
+            }
+
+            public PipId GetOrAddPip(string hash, string name, TimeSpan executionTime)
+                => PipTableBuilder.GetOrAdd(hash, name, executionTime);
+
+            public FileId GetOrAddFile(string name, long sizeInBytes)
+                => FileTableBuilder.GetOrAdd(name, sizeInBytes);
+        }
     }
 }
