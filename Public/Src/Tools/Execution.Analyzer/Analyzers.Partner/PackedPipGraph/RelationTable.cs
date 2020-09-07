aeab31d9352986dc60ea4ea871dae049ef64c15d
Rob Jellinghaus <rjelling@microsoft.com>
2020-09-06 19:29:49 -0700

    RelationTable lives! ...just barely.

--- Public/Src/Tools/Execution.Analyzer/Analyzers.Partner/PackedPipGraph/RelationTable.cs
+++ Public/Src/Tools/Execution.Analyzer/Analyzers.Partner/PackedPipGraph/RelationTable.cs
@@ -2,10 +2,7 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using BuildXL.ToolSupport;
-using BuildXL.Utilities.Configuration;
 
 namespace BuildXL.Execution.Analyzers.PackedPipGraph
 {
@@ -19,9 +16,11 @@ namespace BuildXL.Execution.Analyzers.PackedPipGraph
     /// - m_relations: the collection of all TToIds for all relationships; sorted by TFromId then TToId.
     /// 
     /// For example, if we have FromId 1 related to ToIds 1 and 2, and FromId 3 related to ToIds 1 and 3:
-    /// Values: [2, 0, 2]
-    /// m_offsets: [0, 2, 2]
+    /// Values: [0, 2, 0, 2]
+    /// m_offsets: [0, 0, 2, 2]
     /// m_relations: [1, 2, 1, 3]
+    /// 
+    /// Note that Values and m_offsets both start with 0, as they map to IDs directly.
     /// </remarks>
     public class RelationTable<TFromId, TToId, TFromTable, TToTable> : DerivedTable<TFromId, int, TFromTable>
         where TFromId : unmanaged, Id<TFromId>
@@ -36,11 +35,10 @@ namespace BuildXL.Execution.Analyzers.PackedPipGraph
         /// </summary>
         /// <remarks>
         /// Computed from a scan over Values, which is the per-TFromId count of relations per element.
-        /// Doesn't need to be a SpannableList because we never save this, we recompute it on load.
         /// When building incrementally, this list grows progressively; if this list has fewer elements
-        /// than Count, it means only a prefix of all IDs have been added yet.
+        /// than Count, it means only a prefix of all IDs have had their relations added yet.
         /// </remarks>
-        private readonly List<int> m_offsets;
+        private readonly SpannableList<int> m_offsets;
 
         /// <summary>
         /// List of actual relations.
@@ -55,9 +53,11 @@ namespace BuildXL.Execution.Analyzers.PackedPipGraph
         public RelationTable(TFromTable baseTable, TToTable relatedTable) : base(baseTable)
         {
             RelatedTable = relatedTable;
-            m_offsets = new List<int>(Count + 1);
+            m_offsets = new SpannableList<int>(Count + 1);
             m_offsets.Add(0);
             m_relations = new SpannableList<TToId>();
+
+            Console.WriteLine($"RelationTable() end: {ToFullString()}");
         }
 
         public override void SaveToFile(string directory, string name)
@@ -84,7 +84,7 @@ namespace BuildXL.Execution.Analyzers.PackedPipGraph
         /// <summary>
         /// A span over the actual relations of this particular id.
         /// </summary>
-        public ReadOnlySpan<TToId> Relations(TFromId id)
+        public ReadOnlySpan<TToId> GetRelations(TFromId id)
         {
             int offset = m_offsets[id.FromId()];
             int count = Values[id.FromId()];
@@ -101,6 +101,8 @@ namespace BuildXL.Execution.Analyzers.PackedPipGraph
         /// </remarks>
         public void AddRelations(TFromId id, ReadOnlySpan<TToId> newRelations)
         {
+            Console.WriteLine($"RelationTable.AddRelations start: {ToFullString()}");
+
             // Ensure newRelations are sorted.
             for (int i = 1; i < newRelations.Length; i++)
             {
@@ -117,7 +119,7 @@ namespace BuildXL.Execution.Analyzers.PackedPipGraph
             {
                 // this is the only valid value at which relations can be added.
                 m_offsets.Add(m_offsets[idInt - 1] + Values[idInt - 1]);
-                Values.Add(newRelations.Length);
+                Values[idInt] = newRelations.Length;
 
                 m_relations.AddRange(newRelations);
             }
@@ -126,6 +128,10 @@ namespace BuildXL.Execution.Analyzers.PackedPipGraph
                 throw new InvalidArgumentException(
                     $"Cannot add relations for {idInt} to backing list with m_offsets.Count {m_offsets.Count}, Values.Count {Values.Count}, m_relations.Count {m_relations.Count}");
             }
+
+            Console.WriteLine($"RelationTable.AddRelations end: {ToFullString()}");
         }
+
+        public string ToFullString() => $"Values {Values.ToFullString()}, m_offsets {m_offsets.ToFullString()}, m_relations {m_relations.ToFullString()}";
     }
 }
---
+++ Public/Src/Tools/UnitTests/Analyzers/PackedPipGraphRelationTableTests.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using BuildXL.Execution.Analyzers.PackedPipGraph;
+using System;
+using System.Linq;
+using Test.BuildXL.TestUtilities.Xunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Test.Tool.Analyzers
+{
+    public class PackedPipGraphRelationTableTests : TemporaryStorageTestBase
+    {
+        public PackedPipGraphRelationTableTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void RelationTable_can_store_one_relation()
+        {
+            PackedPipGraph pipGraph = new PackedPipGraph();
+            PackedPipGraph.Builder pipGraphBuilder = new PackedPipGraph.Builder(pipGraph);
+            string hash = "PipHash";
+            string name = "ShellCommon.Shell.ShellCommon.Shell.Merged.Winmetadata";
+            PipId pipId = pipGraphBuilder.PipTableBuilder.GetOrAdd(hash, name, new TimeSpan(0, 5, 0));
+
+            RelationTable<PipId, PipId, PipTable, PipTable> relationTable = 
+                new RelationTable<PipId, PipId, PipTable, PipTable>(pipGraph.PipTable, pipGraph.PipTable);
+
+            XAssert.AreEqual(0, relationTable[pipId]);
+
+            relationTable.AddRelations(pipId, new[] { pipId }.AsSpan());
+
+            XAssert.AreEqual(1, relationTable[pipId]);
+
+            ReadOnlySpan<PipId> relations = relationTable.GetRelations(pipId);
+
+            XAssert.AreEqual(relations[0], pipId);
+        }
+    }
+}
